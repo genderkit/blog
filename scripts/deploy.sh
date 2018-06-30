0c41fd2c11f328bc47fbcd487c353c21f2e926b9
fallax <2036067+fallax@users.noreply.github.com>
2018-06-30 17:09:41 +0000

    Added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Before running this script you must have installed the AWS CLI.
+# On ubuntu, do:
+# 1) sudo apt-get install python-pip python-dev build-essential 
+# 2) pip install --upgrade --user awscli
+# 3) Set up the AWS credentials using aws configure
+
+cd /vagrant
+mkdir live
+cd live
+git clone git@github.com:genderkit/blog.git
+cd blog
+bundle install
+bundle exec jekyll build --config /vagrant/live/blog/_config-live.yml
+aws s3 sync ./_site/ s3://blog.genderkit.org.uk/
+rm -rv /vagrant/live
